127d0e78be772a8e7e1035560e702542f91889cd
rayray1010 <da866810@gmail.com>
2021-10-05 19:24:43 +0800

    feat: add cookies function

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const ephbs = require('express-handlebars')
-const app = express()
 const routes = require('./routes/index')
+const cookieParser = require('cookie-parser')
+const app = express()
 require('./config/mongoose')
 
 //setting express config
 app.engine('hbs', ephbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extended: true }))
-app.use(routes)
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser('123456789'))
+app.use(routes)
 
 app.listen(3000, () => {
   console.log('web server is running on http://localhost:3000/')
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,7 +3,19 @@ const router = express.Router()
 const Login = require('../../models/login')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  let firstName = ''
+  let isLogin = false
+  if (req.signedCookies.firstName) {
+    firstName = req.signedCookies.firstName
+    isLogin = true
+  }
+  res.render('index', { firstName, isLogin })
+})
+router.get('/:message', (req, res) => {
+  let isWrong = true
+  let firstName = ''
+  let isLogin = false
+  res.render('index', { firstName, isLogin, isWrong })
 })
 
 router.post('/login', (req, res) => {
@@ -11,13 +23,22 @@ router.post('/login', (req, res) => {
   Login.findOne({ email, password })
     .then((data) => {
       if (data !== null) {
-        console.log(data)
-        res.render('index', { data })
-      } else res.render('index', { warning: 'please try again!' })
+        res.cookie('firstName', data.firstName, {
+          path: '/',
+          signed: true,
+          maxAge: 600000,
+        }) //set cookie
+        res.redirect('/')
+      } else res.redirect(301, '/error')
     })
     .catch((err) => {
       console.log(err)
     })
+})
+
+router.post('/logout', (req, res) => {
+  res.clearCookie('firstName', { path: '/' })
+  return res.redirect('/')
 })
 
 router.post('/')
